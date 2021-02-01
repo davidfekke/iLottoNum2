3dacb7dd37323e9f4efa226839f6cdc27ed29ed6
David Fekke <david@fekke.com>
2021-01-31 21:09:43 -0500

    Initial Commit

---
+++ Shared/iLottNum2App.swift
@@ -0,0 +1,17 @@
+//
+//  iLottNum2App.swift
+//  Shared
+//
+//  Created by David Fekke on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct iLottNum2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
